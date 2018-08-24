a230603da2953858ad5a90b5240e2c1e8393e5e3
Brandon Addison <addbra001@myuct.ac.za>
2018-08-24 03:41:05 +0200

    final commit, added comments, removed debugging code

--- pca.cpp
+++ pca.cpp
@@ -1,4 +1,4 @@
-// ADDBRA001 Lab 3
+// ADDBRA001 ML Lab 3
 
 #include <iostream>
 #include <fstream>
@@ -14,18 +14,14 @@
 using namespace std;
 using namespace Eigen;
 
-vector <string> places;
-vector <float> x_vals;
-vector <float> y_vals;
 vector < pair< string, vector<float> > > data;
 
 void readfile (string filename) {
 
   string line, input;
-
-
   ifstream in(filename);
 
+  // Skip the first 5 lines
   getline(in, line);
   getline(in, line);
   getline(in, line);
@@ -34,25 +30,23 @@ void readfile (string filename) {
 
   while ( getline(in >> ws, line) ) {
 
-    string place = line;
+    string place = line;  // Place name
     getline(in, line);
     getline(in, line);
-    float x = stof(line);
+    float x = stof(line);  // January Rainfall recorded as float value
     getline(in, line);
-    float y = stof(line);
+    float y = stof(line);  // July Rainfall recorded as float value
     getline(in, line);
     getline(in, line);
     getline(in, line);
 
-    vector <float> vars = {x,y};
-    //pair<float, float> vars (x, y);
-    pair<string, vector<float> > point(place, vars);
+    vector <float> vars = {x,y};  // Jan/July components recorded as co-ordinate value
+    pair<string, vector<float> > point(place, vars);  // pair to keep track of what values belong to what place
     data.push_back(point);
-    //delete vars;
-
   }
 }
 
+// Component Average calculation function
 float getAverage(int component) {
   float sum = 0;
   for (int i = 0; i < data.size(); i++) {
@@ -62,17 +56,14 @@ float getAverage(int component) {
   return avg;
 }
 
+// Variance/Covariance calculation function
 float covar (int c1, int c2) {
   float sum = 0;
-
   for (int i = 0; i < data.size(); i++) {
     sum += (data[i].second[c1] - getAverage(c1))*(data[i].second[c2] - getAverage(c2));
   }
-
   float result = sum/(data.size()-1);
-
   return result;
-
 }
 
 
@@ -80,8 +71,10 @@ int main () {
 
   readfile ("2018-AvgRainfall_mm_.txt");
 
+  // 2x2 Matrix intialised
   Matrix<float, 2, 2> covarMatrix;
 
+  // setting matrix vertices values
   covarMatrix(0,0) = covar(0,0);
   covarMatrix(0,1) = covar(0,1);
   covarMatrix(1,0) = covar(1,0);
